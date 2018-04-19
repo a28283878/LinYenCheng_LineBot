2b38287158f647b33879ffaeec1f32b0b008c59b
Richard Lin <a28283878>
2018-04-19 16:00:45 +0200

    refector

--- main.go
+++ main.go
@@ -34,13 +34,12 @@ func initValues() {
 	channelAccessToken = os.Getenv("ChannelAccessToken")
 
 	locationBtn := linebot.NewMessageTemplateAction("你在哪交換", "你在哪交換")
-	resumeBtn := linebot.NewMessageTemplateAction("給我看履歷", "給我看履歷")
 	skillBtn := linebot.NewMessageTemplateAction("你會哪些東西呢", "你會哪些東西呢")
 	hobbyBtn := linebot.NewMessageTemplateAction("你有哪些興趣呢", "你有哪些興趣呢")
 	travelBtn := linebot.NewMessageTemplateAction("最近文章", "最近文章")
 
 	questionTemplate = linebot.NewButtonsTemplate("https://farm1.staticflickr.com/799/41548719091_313673967f_b.jpg", "這裡有些範例問題呢",
-		"選個看看吧", locationBtn, resumeBtn, skillBtn, travelBtn, hobbyBtn)
+		"選個看看吧", locationBtn, skillBtn, travelBtn, hobbyBtn)
 }
 
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
